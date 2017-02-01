2f51e1246b017ae1e096108b5b0af754d41ee281
joseluisgavilan@outlook.com <joseluis.gavilan@iesmarenostrum.com>
2017-02-01 20:40:10 +0100

    cambio de color

--- Coche/frmEjemplo.Designer.cs
+++ Coche/frmEjemplo.Designer.cs
@@ -153,6 +153,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
             this.ClientSize = new System.Drawing.Size(459, 261);
             this.Controls.Add(this.gbCoche);
             this.Controls.Add(this.gbAlerta);
