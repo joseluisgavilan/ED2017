16d09fc6b055fa5705b47bf98e801125e8aca3ce
joseluisgavilan@outlook.com <joseluis.gavilan@iesmarenostrum.com>
2017-02-01 21:41:28 +0100

    CREAR

--- Coche/frmEjemplo.Designer.cs
+++ Coche/frmEjemplo.Designer.cs
@@ -48,52 +48,47 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(6, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.Size = new System.Drawing.Size(90, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Marca del coche:";
             // 
             // txtMarca
             // 
-            this.txtMarca.Location = new System.Drawing.Point(160, 29);
-            this.txtMarca.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMarca.Location = new System.Drawing.Point(107, 19);
             this.txtMarca.Name = "txtMarca";
-            this.txtMarca.Size = new System.Drawing.Size(178, 26);
+            this.txtMarca.Size = new System.Drawing.Size(120, 20);
             this.txtMarca.TabIndex = 1;
             // 
             // bttCrear
             // 
-            this.bttCrear.Location = new System.Drawing.Point(361, 29);
-            this.bttCrear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bttCrear.Location = new System.Drawing.Point(241, 19);
             this.bttCrear.Name = "bttCrear";
-            this.bttCrear.Size = new System.Drawing.Size(226, 94);
+            this.bttCrear.Size = new System.Drawing.Size(151, 61);
             this.bttCrear.TabIndex = 2;
-            this.bttCrear.Text = "Crear";
+            this.bttCrear.Text = "CREAR";
             this.bttCrear.UseVisualStyleBackColor = true;
             this.bttCrear.Click += new System.EventHandler(this.bttCrear_Click);
             // 
             // udVelocidad
             // 
-            this.udVelocidad.Location = new System.Drawing.Point(160, 69);
-            this.udVelocidad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.udVelocidad.Location = new System.Drawing.Point(107, 45);
             this.udVelocidad.Maximum = new decimal(new int[] {
             180,
             0,
             0,
             0});
             this.udVelocidad.Name = "udVelocidad";
-            this.udVelocidad.Size = new System.Drawing.Size(180, 26);
+            this.udVelocidad.Size = new System.Drawing.Size(120, 20);
             this.udVelocidad.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(-4, 80);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(-3, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 20);
+            this.label2.Size = new System.Drawing.Size(104, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Velocidad del coche";
             // 
@@ -103,21 +98,18 @@
             this.gbAlerta.Controls.Add(this.nUDLimite);
             this.gbAlerta.Controls.Add(this.label3);
             this.gbAlerta.Enabled = false;
-            this.gbAlerta.Location = new System.Drawing.Point(36, 206);
-            this.gbAlerta.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbAlerta.Location = new System.Drawing.Point(24, 134);
             this.gbAlerta.Name = "gbAlerta";
-            this.gbAlerta.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.gbAlerta.Size = new System.Drawing.Size(606, 154);
+            this.gbAlerta.Size = new System.Drawing.Size(404, 100);
             this.gbAlerta.TabIndex = 5;
             this.gbAlerta.TabStop = false;
             this.gbAlerta.Text = "Alertas";
             // 
             // bttComprueba
             // 
-            this.bttComprueba.Location = new System.Drawing.Point(442, 38);
-            this.bttComprueba.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bttComprueba.Location = new System.Drawing.Point(295, 25);
             this.bttComprueba.Name = "bttComprueba";
-            this.bttComprueba.Size = new System.Drawing.Size(112, 45);
+            this.bttComprueba.Size = new System.Drawing.Size(75, 29);
             this.bttComprueba.TabIndex = 2;
             this.bttComprueba.Text = "Comprueba";
             this.bttComprueba.UseVisualStyleBackColor = true;
@@ -125,24 +117,22 @@
             // 
             // nUDLimite
             // 
-            this.nUDLimite.Location = new System.Drawing.Point(254, 48);
-            this.nUDLimite.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nUDLimite.Location = new System.Drawing.Point(169, 31);
             this.nUDLimite.Maximum = new decimal(new int[] {
             120,
             0,
             0,
             0});
             this.nUDLimite.Name = "nUDLimite";
-            this.nUDLimite.Size = new System.Drawing.Size(180, 26);
+            this.nUDLimite.Size = new System.Drawing.Size(120, 20);
             this.nUDLimite.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 48);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(18, 31);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(196, 20);
+            this.label3.Size = new System.Drawing.Size(136, 13);
             this.label3.TabIndex = 0;
             this.label3.Text = "Elige el límite de velocidad:";
             // 
@@ -153,11 +143,9 @@
             this.gbCoche.Controls.Add(this.label1);
             this.gbCoche.Controls.Add(this.label2);
             this.gbCoche.Controls.Add(this.bttCrear);
-            this.gbCoche.Location = new System.Drawing.Point(36, 43);
-            this.gbCoche.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbCoche.Location = new System.Drawing.Point(24, 28);
             this.gbCoche.Name = "gbCoche";
-            this.gbCoche.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.gbCoche.Size = new System.Drawing.Size(606, 154);
+            this.gbCoche.Size = new System.Drawing.Size(404, 100);
             this.gbCoche.TabIndex = 6;
             this.gbCoche.TabStop = false;
             this.gbCoche.Text = "Coche";
@@ -166,23 +154,23 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Coral;
-            this.label4.Location = new System.Drawing.Point(202, 369);
+            this.label4.Location = new System.Drawing.Point(135, 240);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(109, 20);
+            this.label4.Size = new System.Drawing.Size(73, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Último cambio";
             // 
             // frmEjemplo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RoyalBlue;
-            this.ClientSize = new System.Drawing.Size(688, 402);
+            this.ClientSize = new System.Drawing.Size(459, 261);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.gbCoche);
             this.Controls.Add(this.gbAlerta);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmEjemplo";
             this.Text = "AppEjemplo";
             this.Load += new System.EventHandler(this.frmEjemplo_Load);
